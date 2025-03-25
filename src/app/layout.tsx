552175644963d59f664fc3a524a4fa1608bb3692
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-03-25 19:01:58 +0200

    create main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import {  Geist_Mono } from "next/font/google";
 import "./globals.scss";
 import AuthProvider from "./Components/AuthProvider/AuthProvider";
-import Header from "@/app/Components/Header/Header";
+import Header from "./Components/Header/Header";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
