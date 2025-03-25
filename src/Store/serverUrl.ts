6114ac4bd20049f5c14fcc44f7f3eb8e64fb1db0
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-03-25 19:57:34 +0200

    deploy vercel with onRender

--- src/Store/serverUrl.ts
+++ src/Store/serverUrl.ts
@@ -17,7 +17,7 @@ export const serverUrlStore = create<State & Actions>()(
                 if (process.env.NODE_ENV === 'development') {
                     return 'http://localhost:8000/api'; // Local development
                 } else {
-                    return 'https://elif-tech-back-aeaf208a7993.herokuapp.com/api'
+                    return 'https://eliftech-back-v9rr.onrender.com/api'
                 }
             };
             set({serverUrl: getUrl()})
